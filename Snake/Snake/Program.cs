1aa9a38a83026abddf39afea8a86acd21ba9f8a1
Vlasiy <sh.vlas@mail.ru>
2016-03-22 08:06:57 +1000

    ДЗ - эксперименты с классом List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,15 +10,41 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            /*
             Point p1 = new Point(1, 3, '+');
             p1.Draw();
 
             Point p2 = new Point(4, 5, '-');
             p2.Draw();
-            */
 
-            Console.WriteLine("Урок 5 RAM - в процессе изучения");
+            Point p3 = new Point(6, 7, '$');
+            p3.Draw();
+
+            Point p4 = new Point(2, 8, '#');
+            p4.Draw();
+
+            List<char> numList = new List<char>();
+            numList.Add('-');
+            numList.Add('+');
+            numList.Add('#');
+
+            char x = numList[0];
+            char y = numList[1];
+            char z = numList[2];
+
+            foreach (char i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(2);
+
+            List<Point> plist = new List<Point>();
+
+            plist.Add(p1);
+            plist.Add(p2);
+            plist.Add(p3);
+            plist.Add(p4);
+
             Console.ReadLine();
         }
     }
