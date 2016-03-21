51be818b6486417a0eda2e9c6b3f4406d26329ce
Vlasiy <sh.vlas@mail.ru>
2016-03-22 05:47:14 +1000

    ДЗ

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,12 +10,15 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            /*
             Point p1 = new Point(1, 3, '+');
             p1.Draw();
 
             Point p2 = new Point(4, 5, '-');
             p2.Draw();
+            */
 
+            Console.WriteLine("Урок 5 RAM - в процессе изучения");
             Console.ReadLine();
         }
     }
